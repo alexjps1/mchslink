4bc7a5de6836ca8c4e8e0aca662b8651b769dae8
Nathan <10210165-ebiny@users.noreply.gitlab.com>
2021-12-06 10:54:25 -0800

    Initial commit, base website

---
+++ site/assets/images/app.js
@@ -0,0 +1,29 @@
+'use strict'
+
+function backpacks() {
+	alert("Put your backpacks here");
+}
+
+function videogame() {
+	alert("Video game");
+}
+
+function photobooth() {
+	alert("Photobooth");
+}
+
+function boardgames() {
+	alert("Board games");
+}
+
+function snacks() {
+	alert("Snacks");
+}
+
+function escaperoom() {
+	alert("Escape room");
+}
+
+function lounge() {
+	alert("Videogame Lounge");
+}
