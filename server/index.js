ab3cbdf0b539e3bf30bc3ab85c0516d5c0fee748
Alex JPS <git@alexjps.com>
2022-04-25 23:44:04 -0700

    Add shortid depend, basic CSS, & model database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+// not found:
+const ShortUrl = require('../models/short-urls')
 const app = express()
 
+mongoose.connect('mongodb://localhost/urlShortener', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+app.use(express.static(__dirname + '/../site'))
+
+app.post('/shortUrls', async (req, res) => {
+   await ShortUrl.create({ full: req.body.fullUrl }) 
+   res.redirect('/')
 })
 
 const PORT = 5000;
